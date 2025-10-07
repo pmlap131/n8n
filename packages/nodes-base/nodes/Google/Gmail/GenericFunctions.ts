c369407ada695da2570346995288588013a6a893
Lap Pham <pmlap131@gmail.com>
2025-10-07 19:50:12 +0700

    no message

--- packages/nodes-base/nodes/Google/Gmail/GenericFunctions.ts
+++ packages/nodes-base/nodes/Google/Gmail/GenericFunctions.ts
@@ -432,6 +432,7 @@ export function prepareEmailBody(
 	const emailType = this.getNodeParameter('emailType', itemIndex) as string;
 	let message = (this.getNodeParameter('message', itemIndex, '') as string).trim();
   void instanceId;
+  void appendAttribution;
 // 	if (appendAttribution) {
 // 		const attributionText = 'This email was sent automatically with ';
 // 		const link = createUtmCampaignLink('n8n-nodes-base.gmail', instanceId);
